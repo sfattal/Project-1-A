a6c63ef67c6d37e5cf6c2ded52cee16808dfc665
aocny <aocny@aol.com>
2018-11-12 20:41:17 -0500

    first draft html

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # Project-1-A
 
-<<<<<<< HEAD
 aaaammmmyyyy
 # Test Update - Lennox
-=======
 
 Team Members:
 Sherif Fattal
 
 # Test Update - Lennox
 
->>>>>>> master
---
+++ app.js
@@ -0,0 +1,13 @@
+
+// 2. Button for adding symptoms
+$("#add-symptom-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    var symp1 = $("#symptom1-input").val().trim();
+    var symp2 = $("#symptom2-input").val().trim();
+    var symp3 = $("#symptom3-input").val().trim();
+   
+  
+
+
