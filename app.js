47195652015afa0f46b45fab577ab6ed8834aabd
aocny <aocny@aol.com>
2018-11-17 11:03:55 -0500

    trying to pull

--- app.js
+++ app.js
@@ -2,12 +2,7 @@
 // 2. Button for adding symptoms
 $("#add-symptom-btn").on("click", function(event) {
     event.preventDefault();
-  
-    // Grabs user input
-    var symp1 = $("#symptom1-input").val().trim();
-    var symp2 = $("#symptom2-input").val().trim();
-    var symp3 = $("#symptom3-input").val().trim();
-   
-  
 
-
+    $(".form-group").append("<input/>")
+    
+})
